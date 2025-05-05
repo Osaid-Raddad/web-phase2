0a5930477f2a542f730f87f5810ce8f98e10725c
Osaid-Raddad <osaidislam1@gmail.com>
2025-05-05 22:01:31 +0300

    solve import

--- src/components/auth/signIn/SignIn.jsx
+++ src/components/auth/signIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from './SignIn.module.css';
+import styles from './signIn.module.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast, Slide } from 'react-toastify';
 
